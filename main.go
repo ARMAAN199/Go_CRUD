53ac99ab60da1922998281131d9cc617ad81e293
ARMAAN199 <arman0432.cse19@chitkara.edu.in>
2022-05-23 12:56:50 +0530

    reg

--- main.go
+++ main.go
@@ -1,40 +1,41 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"log"
 	"encoding/json"
-	"math/rand"
-	"strconv"
+	"fmt"
+	"log"
+	"net/http"
+
+	// "math/rand"
+
 	"github.com/gorilla/mux"
 )
 
-
-struct Book {
-	ID string `json:"id"`
-	Title string `json:"title"`
-	Year string `json:"year"`
+type Book struct {
+	ID     string  `json:"id"`
+	Title  string  `json:"title"`
+	Year   string  `json:"year"`
 	Author *Author `json:"author"`
 }
 
-struct Author {
-	ID string `json:"id"`
+type Author struct {
+	ID        string `json:"id"`
 	Firstname string `json:"firstname"`
-	Lastname string `json:"lastname"`
+	Lastname  string `json:"lastname"`
 }
-
 
 var books []Book
 
 func main() {
 
-	books = books.append(Book{ID: "1", Title: "Book One", Year: "2018", Author: &Author{ID: "1", Firstname: "John", Lastname: "Doe"}})
-	books = books.append(Book{ID: "2", Title: "Book Two", Year: "2019", Author: &Author{ID: "2",Firstname: "John2", Lastname: "Doe2" }})
+	books = append(books, Book{ID: "1", Title: "Book One", Year: "2018", Author: &Author{ID: "1", Firstname: "John", Lastname: "Doe"}})
+	books = append(books, Book{ID: "2", Title: "Book Two", Year: "2019", Author: &Author{ID: "2", Firstname: "John2", Lastname: "Doe2"}})
 	router := mux.NewRouter()
+	http.Handle("/", router)
+	// router.HandleFunc("/", checkserver).Methods("GET")
 	router.HandleFunc("/api/books", getBooks).Methods("GET")
 	// router.HandleFunc("/api/books/{id}", getBook).Methods("GET")
-	// router.HandleFunc("/api/addbook", createBook).Methods("POST")
+	router.HandleFunc("/api/addbook", createBook).Methods("POST")
 	// router.HandleFunc("/api/updatebook/{id}", updateBook).Methods("PUT")
 	router.HandleFunc("/api/deletebook/{id}", deleteBook).Methods("DELETE")
 
@@ -45,33 +46,41 @@ func main() {
 	}
 }
 
-func getBooks(w http.ResponseWriter, r *http.Request)
-{
+func checkserver(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintf(w, "Hello, %s!", r.URL.Path)
+}
+
+func getBooks(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(books)
 }
 
-func deleteBook(w http.ResponseWriter, r *http.Request)
-{
+func deleteBook(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	params := mux.Vars(r)
-	ind int;
+	var ind int
 	for index, book := range books {
-		if book.ID == params[id]{
+		if book.ID == params["id"] {
 			ind = index
 		}
 	}
-	books = append ( books[:ind], books[ind+1:]...)
+	books = append(books[:ind], books[ind+1:]...)
 	json.NewEncoder(w).Encode(books)
 }
 
+func createBook(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	log.Println(params)
+	// books = append(books, Book{ID: strconv.Itoa(len(books)) , Title: params["name"], Year: "2018", Author: &Author{ID: "1", Firstname: "John", Lastname: "Doe"}})
+	json.NewEncoder(w).Encode(books)
+}
 
 // func main(){
 // 	fileserver := http.FileServer(http.Dir("./static"))
 // 	http.Handle("/", fileserver)
 // 	http.HandleFunc("/form", formhandle)
 // 	http.HandleFunc("/hello", hellohandle)
-
 
 // 	log.Println("Listening... on port `8080`")
 // 	err := http.ListenAndServe(":8080", nil)
